06ccb980545733d96865d6d15fd428f8b8359fd8
Jaeson Fan <sergefan@live.com>
2025-02-21 22:17:52 +0800

    fix auth module

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import { authConfig } from '@/auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
 import postgres from 'postgres';
 import bcrypt from 'bcrypt';
 
@@ -11,7 +12,7 @@ async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT *
                                    FROM users
-                                   WHERE email = $(email)`;
+                                   WHERE email = ${email}`;
 
     return user[0];
   } catch (error) {
